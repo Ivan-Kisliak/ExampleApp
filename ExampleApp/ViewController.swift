ebdc3ab08add22feae4360c0594c20a926a9a988
Ivan <ivangodno86@gmail.com>
2024-09-17 20:24:27 +0300

    Add textLabel in ViewController

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -10,21 +10,31 @@ import UIKit
 class ViewController: UIViewController {
 
     private let helper = Helper()
+    private let textlabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .green
         updateNumbers()
-
+        
+        setupLabel()
+        view.addSubview(textlabel)
     }
     
-    func updateNumbers() {
+    private func updateNumbers() {
         helper.addNumber(Int.random(in: 1...10))
         
         for number in helper.getNumbers() {
             print(number)
         }
+    }
+    
+    private func setupLabel() {
+        let firstNumber = helper.getNumbers().first
+        textlabel.text = firstNumber?.formatted()
+        textlabel.font = .systemFont(ofSize: 30, weight: .bold)
+        textlabel.textColor = .red
+        textlabel.frame = CGRect(x: 30, y: 30, width: 100, height: 50)
     }
 
 }
